230cc0d9e8fbccd63dab8a536c08c8d42e7b27e8
dreamless21 <80602763+dreamless21@users.noreply.github.com>
2022-06-28 15:31:12 +0300

    Delete test_django/articles directory

--- test_django/articles/static_package.py
+++
@@ -1,9 +0,0 @@
-
-card_dict = {
-                'Red': [('DIAMONDS', 'HEARTS'), 2],
-                'Black': [('SPADES', 'CLUBS'), 2],
-                'Diamonds': [('DIAMONDS',), 4],
-                'Hearts': [('HEARTS',), 4],
-                'Clubs': [('CLUBS',), 4],
-                'Spades': [('SPADES',), 4]
-            }
